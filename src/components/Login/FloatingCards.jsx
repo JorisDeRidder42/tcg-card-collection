ea26a5929d18952529bfe0019e16b9d9c92a2eff
jorisderidder42 <jorisderidder@outlook.be>
2026-07-26 22:26:24 +0200

    feat: fixed cards background

--- src/components/Login/FloatingCards.jsx
+++ src/components/Login/FloatingCards.jsx
@@ -19,31 +19,12 @@ const cards = [
 ];
 
 const FloatingCards = () => {
-
-  const { data: cards } = useFetchList("/cards");
-
-
-  const [displayCards, setDisplayCards] = useState([]);
-
-
-  useEffect(() => {
-
-    if (!cards?.length) return;
-
-    const randomCards = [...cards]
-      .sort(() => Math.random() - 0.5)
-      .slice(0,12);
-
-    setDisplayCards(randomCards);
-  },[cards]);
-
   return (
     <>
-      {displayCards.map((card,index)=>(
-        <div key={card.id} className={`floating-card card-${index}`}>
-
+      {cards.map((card,index)=>(
+        <div key={index} className={`floating-card card-${index}`}>
           <img
-            src={`${card.image}/high.png`}
+            src={card}
             alt="card images"
           />
         </div>
